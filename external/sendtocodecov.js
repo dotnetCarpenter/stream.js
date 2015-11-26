7f7e76faca6725a926910cf9762b5cd9f70d3edb
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-11-26 13:52:04 +0100

    learning to use stream.js

--- external/sendtocodecov.js
+++ external/sendtocodecov.js
@@ -1,24 +1,37 @@
 #!/usr/bin/env node
 
+var Stream = require('../lib/index');
 var fs = require('fs');
 var spawn = require('child_process').spawn;
 var path = require('path');
-
-fs.readFile(path.resolve(__dirname, '..', 'coverage/coverage.json'), 'utf8', function(err, data) {
+var coveragePath = path.resolve(__dirname, '..', 'coverage/coverage.json');
+/*fs.readFile(coveragePath, 'utf8', function(err, data) {
   if (err) {
     console.error(err);
     console.error('coverage data not found/read');
     process.exit(1);
   }
   pipeToCodeCov(data);
-});
+});*/
+var coverageData = fs.createReadStream(coveragePath);
+var stream = new Stream();
+coverageData.on('data', stream.iterate);
+coverageData.read();
+function readData(data) {
+  return new Stream(data, promise);
+  function promise(data) {
+    return readData(data);
+  }
+}
+stream.print();
+console.log(stream);
 
 function pipeToCodeCov(json) {
-  var codecov = 'node_modules/.bin/codecov';
+  var codecov = 'external/testsendtocodecov.js';//'node_modules/.bin/codecov';
   var child;
   var node = process.execPath;
   var codecov = path.resolve(__dirname, '..', codecov)
-  try {
+  try { // TODO: test that we actually recieve/catch exeptions
     child = spawn(node, [codecov]);
   } catch (err) {
     console.error('codecov lib is not accessible');
