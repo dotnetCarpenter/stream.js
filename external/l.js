bcbfa02e28944c1f1b82e06fc8dac67195dc9ad6
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-11-27 21:35:47 +0100

    learned how to use stream.js as a queue

--- external/l.js
+++ external/l.js
@@ -1,6 +1,9 @@
+"use strict"
+
 var Stream = require("../lib/index.js")
 var log = console.log
-var s = Stream.make(1,2,3)
+
+/*var s = Stream.make(1,2,3)
 s.print()
 log("add 'hello'")
 s = s.append(Stream.make('hello'))
@@ -8,3 +11,17 @@ s.print()
 log("append 'world'")
 s = s.add(Stream.make('world'))
 s.print()
+*/
+let queue = new Stream
+let l1 = [1,2,3]
+let l2 = [3,2,1]
+queue = queue.append(rec(l1))
+queue = queue.append(rec(l2))
+queue.print()
+
+function rec(list) {
+	if(list.length === 0) return new Stream
+	let x = list[0],
+			r = list.slice(1)
+	return new Stream(x, () => rec(r))
+}
