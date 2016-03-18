8bc040c5e48950149ab842d286a974d89323e638
dotnetcarpenter <jon.ronnenberg@gmail.com>
2016-03-18 09:52:46 +0100

    memory leak test stub

---
+++ memleak.js
@@ -0,0 +1,25 @@
+const util = require('util');
+const streamjs = require('./lib/stream.js')
+const l = console.log
+const numbers = streamjs.makeNaturalNumbers()
+
+l(util.inspect(process.memoryUsage()))
+
+l(numbers.item(100))
+
+l(util.inspect(process.memoryUsage()))
+
+l(numbers.item(1000))
+
+l(util.inspect(process.memoryUsage()))
+
+function mapBytes(table) {
+  const retTable = {}
+  //TODO: interpred each numeric value in *table* as bytes and
+  //      convert it upwards in unites so that the number has
+  //      maximum 4 digits. Fix the number with 2 decimals.
+  return retTable
+}
+
+function mapCollection(collection) {}
+function eachCollection(collection) {}
